555a0bb34c8e0637f42f0f6709fa16fdee930dd0
Kaiqiang Huang <kaiqianghuang666@gmail.com>
2021-08-16 22:38:16 +0100

    fix scripts and split path

--- scripts/run_ucf101_tfvaegan.py
+++ scripts/run_ucf101_tfvaegan.py
@@ -2,6 +2,7 @@ import os
 # Need to check: zsl/gzsl, nz/attSize, nclass_all, nepoch
 # Will do:
 # 1. Implement gzsl classifier without using OD (--gzsl)
+# 2. argparse: --zsl; --gzsl; --gzsl_od
 
 os.system('''python /content/kg_gnn_gan/train_tfvaegan.py \
 --nclass_all 101 --dataset ucf101 \
@@ -9,7 +10,7 @@ os.system('''python /content/kg_gnn_gan/train_tfvaegan.py \
 --splits_path ucf101_semantics --split 2 \
 --action_embedding i3d --class_embedding wv \
 --nepoch 3 --batch_size 64 \
---syn_num 600 --preprocessing --cuda --gammaD 10 --gammaG 10 \
+--syn_num 150 --preprocessing --cuda --gammaD 10 --gammaG 10 \
 --ngh 4096 --ndh 4096 --lambda1 10 --critic_iter 5 \
 --nz 300 --attSize 300 --resSize 8192 --lr 0.0001 \
 --encoded_noise --workers 8 \
