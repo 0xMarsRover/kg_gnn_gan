cb9f670d5e0c77acda25d72b18caa9af78554e67
Kaiqiang Huang <kaiqianghuang666@gmail.com>
2021-08-21 09:16:12 +0100

    update hmdb51 script

--- scripts/run_hmdb51_tfvaegan.py
+++ scripts/run_hmdb51_tfvaegan.py
@@ -28,7 +28,7 @@ for n in range(1, 2):
     --dataroot /content/drive/MyDrive/colab_data/action_datasets \
     --splits_path hmdb51_semantics --split {split} \
     --action_embedding i3d --resSize 8192 \
-    --class_embedding avg_img_googlenet --nz 1024 --attSize 1024 \
+    --class_embedding avg_img_googlenet_me --nz 1024 --attSize 1024 \
     --nepoch 25 --batch_size 64 --syn_num 600 \
     --preprocessing --cuda --gammaD 10 --gammaG 10 \
     --ngh 4096 --ndh 4096 --lambda1 10 --critic_iter 5 \
