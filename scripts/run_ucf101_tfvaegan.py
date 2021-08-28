34c28bd5678e961cb90b16a58fdb85f107f0d448
Kaiqiang Huang <kaiqianghuang666@gmail.com>
2021-08-28 14:11:03 +0100

    update

--- scripts/run_ucf101_tfvaegan.py
+++ scripts/run_ucf101_tfvaegan.py
@@ -39,9 +39,10 @@ import os
 #           ii. confusion matrix
 
 
-class_embedding = ['action_class_w2v', 'avg_desc_w2v', 'avg_img_googlenet', 'avg_img_googlenet_me', 'avg_img_resnet18',
-                   'avg_img_resnet50', 'avg_img_resnet101']
-for c in class_embedding:
+class_embedding = {'action_class_w2v': 300, 'avg_desc_w2v': 300,
+                   'avg_img_googlenet': 1024, 'avg_img_googlenet_me': 1024,
+                   'avg_img_resnet18': 512, 'avg_img_resnet50': 2048, 'avg_img_resnet101': 2048}
+for c, dim in class_embedding.items():
     for n in range(1, 2):
         # n = n + 1
         os.system('''CUDA_LAUNCH_BLOCKING=1 python /content/kg_gnn_gan/train_tfvaegan.py \
@@ -49,10 +50,10 @@ for c in class_embedding:
         --dataroot /content/drive/MyDrive/colab_data/action_datasets \
         --splits_path ucf101_semantics --split {split} \
         --action_embedding i3d --resSize 8192 \
-        --class_embedding {semantics} --nz 1024 --attSize 1024 \
+        --class_embedding {semantics} --nz {semantics_dimension} --attSize {semantics_dimension} \
         --nepoch 100 --batch_size 64 --syn_num 400 \
         --preprocessing --cuda --gammaD 10 --gammaG 10 \
         --ngh 4096 --ndh 4096 --lambda1 10 --critic_iter 5 \
         --lr 0.0001 --workers 8 --encoded_noise  \
         --recons_weight 0.1 --feedback_loop 2 --a2 1 --a1 1 \
-        --feed_lr 0.00001 --dec_lr 0.0001'''.format(split=n, semantics=c))
+        --feed_lr 0.00001 --dec_lr 0.0001'''.format(split=n, semantics=c, semantics_dimension=dim))
