42122279afd0ec6b9674bc14eba363716c14fe56
Kaiqiang Huang <kaiqianghuang666@gmail.com>
2021-09-21 12:11:10 +0100

    update

--- dual/run_hmdb51_tfvaegan_dual.py
+++ dual/run_hmdb51_tfvaegan_dual.py
@@ -17,14 +17,15 @@ for c_t, dim_t in class_embedding_text.items():
     for c_i, dim_i in class_embedding_image.items():
         for n in range(1, 2):
             # n = n + 1
-            os.system('''CUDA_LAUNCH_BLOCKING=1 python /content/kg_gnn_gan/dual/train_tfvaegan_dual.py \
+            #CUDA_LAUNCH_BLOCKING=1
+            os.system('''python /content/kg_gnn_gan/dual/train_tfvaegan_dual.py \
             --dataset hmdb51 --nclass_all 51 --zsl --manualSeed 806 \
             --dataroot /content/drive/MyDrive/colab_data/action_datasets \
             --splits_path hmdb51_semantics --split {split} --combined_syn concat\
             --action_embedding i3d --resSize 8192 \
             --class_embedding_text {semantics_t} --nz_text {semantics_dimension_t} --attSize_text {semantics_dimension_t} \
             --class_embedding_image {semantics_i} --nz_image {semantics_dimension_i} --attSize_image {semantics_dimension_i} \
-            --nepoch 100 --batch_size 256 --syn_num 800 \
+            --nepoch 100 --batch_size 64 --syn_num 800 \
             --preprocessing --cuda --gammaD 10 --gammaG 10 \
             --ngh 4096 --ndh 4096 --lambda1 10 --critic_iter 5 \
             --lr 0.0001 --workers 8 --encoded_noise  \
