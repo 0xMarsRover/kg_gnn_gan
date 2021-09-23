141e0ef187aa867a0e17fc4f5c3d1c25a2693fa8
Kaiqiang Huang <kaiqianghuang666@gmail.com>
2021-09-23 19:42:27 +0100

    update

--- scripts/run_ucf101_tfvaegan.py
+++ scripts/run_ucf101_tfvaegan.py
@@ -51,10 +51,11 @@ class_embedding = {'action_class_w2v': 300, 'avg_desc_w2v': 300,
                    'avg_img_googlenet': 1024, 'avg_img_googlenet_me': 1024, 'avg_img_resnet101': 2048}
 '''
 
-class_embedding = {'action_class_w2v': 300}
+class_embedding = {'action_class_w2v': 300, 'avg_desc_w2v': 300,
+                   'avg_img_googlenet_me': 1024, 'avg_img_resnet101': 2048}
 
 for c, dim in class_embedding.items():
-    for n in range(1, 6):
+    for n in range(6, 11):
         # n = n + 1
         os.system('''CUDA_LAUNCH_BLOCKING=1 python /content/kg_gnn_gan/train_tfvaegan.py \
         --dataset ucf101 --nclass_all 101 --zsl --manualSeed 806 \
