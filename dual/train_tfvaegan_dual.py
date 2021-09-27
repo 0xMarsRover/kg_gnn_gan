416a59451f7197e56463822fbc1ce6927493ace4
Kaiqiang Huang <kaiqianghuang666@gmail.com>
2021-09-27 15:14:27 +0100

    update

--- dual/train_tfvaegan_dual.py
+++ dual/train_tfvaegan_dual.py
@@ -487,10 +487,14 @@ for epoch in range(0, opt.nepoch):
     #(25*800, 8192)
     #print("syn_feature_text_shape: ", syn_feature_text.shape)
 
-
     if opt.combined_syn == 'concat':
         syn_feature = torch.cat((syn_feature_text, syn_feature_image), 1)
         print("syn_feature shape", syn_feature.shape)
+
+    elif opt.combined_syn == 'avg':
+        syn_feature = (syn_feature_text + syn_feature_image) / 2
+        print("syn_feature shape", syn_feature.shape)
+
     else:
         print("Please choose the correct combination approaches.")
 
