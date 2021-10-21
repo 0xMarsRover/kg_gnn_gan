bd1cca7962d9560e7b9e52e290ddcbfefad519e0
Kaiqiang Huang <kaiqianghuang666@gmail.com>
2021-10-21 13:59:18 +0100

    single GAN update

--- scripts/run_ucf101_tfvaegan.py
+++ scripts/run_ucf101_tfvaegan.py
@@ -54,7 +54,7 @@ class_embedding = {'action_class_w2v': 300, 'avg_desc_w2v': 300,
 class_embedding = {'action_class_w2v': 300}
 
 for c, dim in class_embedding.items():
-    for n in range(11, 31):
+    for n in range(21, 31):
         # n = n + 1
         os.system('''CUDA_LAUNCH_BLOCKING=1 python /content/kg_gnn_gan/train_tfvaegan.py \
         --dataset ucf101 --nclass_all 101 --zsl --manualSeed 806 \
