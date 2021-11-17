d67ccf3c1bfc228c895e4e54cacaa402b013b0a2
Kaiqiang Huang <kaiqianghuang666@gmail.com>
2021-11-17 01:07:32 +0000

    update

--- script_dual_ucf101.sh
+++ script_dual_ucf101.sh
@@ -5,7 +5,7 @@
 #SBATCH --mem=32768
 #SBATCH --partition GpuQ
 #SBATCH --nodes 1
-#SBATCH --time=02:00:00
+#SBATCH --time=02-00
 #SBATCH --cpus-per-task=8
 #SBATCH --mail-user=kaiqiang.x.huang@mytudublin.ie
 #SBATCH --mail-type=ALL,TIME_LIMIT_80
