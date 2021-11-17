417382aea5e9961aafd3972d06efd6e8536a98b1
Kaiqiang Huang <kaiqianghuang666@gmail.com>
2021-11-17 00:12:32 +0000

    update

--- dual/run_ucf101_tfvaegan_dual.py
+++ dual/run_ucf101_tfvaegan_dual.py
@@ -10,12 +10,12 @@ class_embedding = {'action_class_w2v': 300, 'avg_desc_w2v': 300, 'fwv_k1_desc': 
                    'avg_img_resnet18': 512, 'avg_img_resnet50': 2048, 'avg_img_resnet101': 2048}
 '''
 
-class_embedding_text = {'action_class_w2v': 300}
+class_embedding_text = {'avg_desc_w2v': 300}
 class_embedding_image = {'avg_img_resnet101': 2048}
 
 for c_t, dim_t in class_embedding_text.items():
     for c_i, dim_i in class_embedding_image.items():
-        for n in range(1, 6):
+        for n in range(19, 20):
             # n = n + 1
             os.system('''CUDA_LAUNCH_BLOCKING=1 python /ichec/home/users/kaiqiang/kg_gnn_gan/dual/train_tfvaegan_dual.py \
             --dataset ucf101 --nclass_all 101 --zsl --manualSeed 806 \
@@ -24,7 +24,7 @@ for c_t, dim_t in class_embedding_text.items():
             --action_embedding i3d --resSize 8192 \
             --class_embedding_text {semantics_t} --nz_text {semantics_dimension_t} --attSize_text {semantics_dimension_t} \
             --class_embedding_image {semantics_i} --nz_image {semantics_dimension_i} --attSize_image {semantics_dimension_i} \
-            --nepoch 100 --batch_size 64 --syn_num 400 \
+            --nepoch 3 --batch_size 64 --syn_num 400 \
             --preprocessing --cuda --gammaD 10 --gammaG 10 \
             --ngh 4096 --ndh 4096 --lambda1 10 --critic_iter 5 \
             --lr 0.0001 --workers 8 --encoded_noise  \
