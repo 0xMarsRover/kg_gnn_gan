3ef95a195c654419e4e1da6e1d598c660e3c6215
Kaiqiang Huang <kaiqianghuang666@gmail.com>
2021-11-21 00:13:46 +0000

    add bash scripts

---
+++ script_dual_hmdb51.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+#SBATCH --job-name=dual_hmdb51
+#SBATCH --account=tud01
+#SBATCH --mem=32768
+#SBATCH --partition GpuQ
+#SBATCH --nodes 1
+#SBATCH --time=02-00
+#SBATCH --cpus-per-task=8
+#SBATCH --mail-user=kaiqiang.x.huang@mytudublin.ie
+#SBATCH --mail-type=ALL,TIME_LIMIT_80
+#SBATCH --output=res_dual_hmdb51.out
+
+module load conda/2
+module load cuda/11.2
+
+source activate /ichec/home/users/kaiqiang/py39
+
+echo "Dual GAN with different classifier for HMDB51 (multi-GPU)"
+
+time python ./dual/run_hmdb51_tfvaegan_dual.py
---
+++ script_dual_ucf101.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+#SBATCH --job-name=dual_ucf101
+#SBATCH --account=tud01
+#SBATCH --mem=32768
+#SBATCH --partition GpuQ
+#SBATCH --nodes 1
+#SBATCH --time=02-00
+#SBATCH --cpus-per-task=8
+#SBATCH --mail-user=kaiqiang.x.huang@mytudublin.ie
+#SBATCH --mail-type=ALL,TIME_LIMIT_80
+#SBATCH --output=res_dual_ucf101.out
+
+module load conda/2
+module load cuda/11.2
+
+source activate /ichec/home/users/kaiqiang/py39
+
+echo "Dual GAN with different classifier for UCF101 (multi-GPU)"
+
+time python ./dual/run_ucf101_tfvaegan_dual.py
