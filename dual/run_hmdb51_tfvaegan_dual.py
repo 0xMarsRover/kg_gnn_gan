8ed9f0ac798eba0a045c924ef61dc8803db202fa
kaiqiangh <kaiqianghuang666@gmail.com>
2021-11-21 12:38:13 +0000

    bug

--- dual/run_hmdb51_tfvaegan_dual.py
+++ dual/run_hmdb51_tfvaegan_dual.py
@@ -15,7 +15,7 @@ class_embedding = {'action_class_w2v': 300, 'avg_desc_w2v': 300, 'fwv_k1_desc': 
 #       --resultsroot /content/drive/MyDrive/colab_data/KG_GCN_GAN
 
 # Kay:
-#       python /ichec/home/users/kaiqiang/kg_gnn_gan/dual/train_tfvaegan_dual.py
+#       python /ichec/home/users/kaiqiang/kay_classifier_dual_gan/dual/train_tfvaegan_dual.py
 #       --dataroot /ichec/work/tud01/kaiqiang/action_datasets
 #       --resultsroot /ichec/home/users/kaiqiang/kg_gnn_gan/dual
 
