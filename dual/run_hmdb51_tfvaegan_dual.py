2dda88c141666dcdcadd18031b987d26a379622b
Kaiqiang Huang <kaiqianghuang666@gmail.com>
2021-11-21 00:25:39 +0000

    update

--- dual/run_hmdb51_tfvaegan_dual.py
+++ dual/run_hmdb51_tfvaegan_dual.py
@@ -25,7 +25,7 @@ class_embedding_image = {'avg_img_googlenet_me': 1024}
 
 for c_t, dim_t in class_embedding_text.items():
     for c_i, dim_i in class_embedding_image.items():
-        for n in range(1, 2):
+        for n in range(1, 5):
             # n = n + 1
             os.system('''CUDA_LAUNCH_BLOCKING=1 python /ichec/home/users/kaiqiang/kay_classifier_dual_gan/dual/train_tfvaegan_dual.py \
             --dataset hmdb51 --nclass_all 51 --zsl --manualSeed 806 \
