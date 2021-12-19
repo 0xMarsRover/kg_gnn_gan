2d2a31ffff8f0fbbb8dd7ae4b3c19ee7b48fa1eb
kaiqiangh <kaiqianghuang666@gmail.com>
2021-12-19 22:46:26 +0000

    exp hmdb51 syn_data 1400 （16-30） on KAY

--- script_dual_hmdb51.sh
+++ script_dual_hmdb51.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-#SBATCH --job-name=hmdb_1600
+#SBATCH --job-name=hmdb_1400
 #SBATCH --account=tud01
 #SBATCH --mem=65536
 #SBATCH --partition GpuQ
@@ -9,7 +9,7 @@
 #SBATCH --cpus-per-task=8
 #SBATCH --mail-user=kaiqiang.x.huang@mytudublin.ie
 #SBATCH --mail-type=ALL,TIME_LIMIT_80
-#SBATCH --output=res_dual_hmdb51_1600.out
+#SBATCH --output=res_dual_hmdb51_1400.out
 
 module load conda/2
 module load cuda/11.2
@@ -17,6 +17,6 @@ module load cuda/11.2
 source activate /ichec/home/users/kaiqiang/py39
 
 echo "Dual GAN with different classifier for HMDB51 (multi-GPU)"
-echo "The number of syn_data is 1600"
+echo "The number of syn_data is 1400"
 
 time python ./dual/run_hmdb51_tfvaegan_dual.py
