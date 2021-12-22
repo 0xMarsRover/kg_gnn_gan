769f7cd8e1821d91e9a6af53f50447c8e0223e93
kaiqiangh <kaiqianghuang666@gmail.com>
2021-12-22 23:50:06 +0000

    exp ucf syn_data 1000 （1-10） on KAY

--- script_dual_ucf101.sh
+++ script_dual_ucf101.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-#SBATCH --job-name=ucf_800
+#SBATCH --job-name=ucf_1000
 #SBATCH --account=tud01
 #SBATCH --mem=65536
 #SBATCH --partition GpuQ
@@ -9,7 +9,7 @@
 #SBATCH --cpus-per-task=8
 #SBATCH --mail-user=kaiqiang.x.huang@mytudublin.ie
 #SBATCH --mail-type=ALL,TIME_LIMIT_80
-#SBATCH --output=res_dual_ucf101_800.out
+#SBATCH --output=res_dual_ucf101_1000.out
 
 module load conda/2
 module load cuda/11.2
@@ -17,6 +17,6 @@ module load cuda/11.2
 source activate /ichec/home/users/kaiqiang/py39
 
 echo "Dual GAN with different classifier for UCF101 (multi-GPU)"
-echo "The number of syn_data is 800"
+echo "The number of syn_data is 1000"
 
 time python ./dual/run_ucf101_tfvaegan_dual.py
