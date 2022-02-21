3d8cad6868e35161f37aed128b32f1618043d445
Kaiqiang <kaiqianghuang666@gmail.com>
2022-02-21 16:25:44 +0000

    gzsl_od hmdb51

--- script_dual_hmdb51.sh
+++ script_dual_hmdb51.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-#SBATCH --job-name=hmdb_1600
+#SBATCH --job-name=hmdb_gzsl_od_800
 #SBATCH --account=tud01
 #SBATCH --mem=65536
 #SBATCH --partition GpuQ
@@ -9,14 +9,14 @@
 #SBATCH --cpus-per-task=8
 #SBATCH --mail-user=kaiqiang.x.huang@mytudublin.ie
 #SBATCH --mail-type=ALL,TIME_LIMIT_80
-#SBATCH --output=res_dual_hmdb51_1600.out
+#SBATCH --output=res_dual_hmdb51_gzsl_od_800.out
 
 module load conda/2
 module load cuda/11.2
 
 source activate /ichec/home/users/kaiqiang/py39
 
-echo "Dual GAN with different classifier for HMDB51 (multi-GPU)"
-echo "The number of syn_data is 1600"
+echo "GZSL_OD: Dual GAN with different classifier for HMDB51 (multi-GPU)"
+echo "The number of syn_data is 800"
 
 time python ./dual/run_hmdb51_tfvaegan_dual.py
