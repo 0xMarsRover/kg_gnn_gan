c5bedeb284478563392e735f7faaef471a08487f
Kaiqiang <kaiqianghuang666@gmail.com>
2022-05-01 22:04:10 +0100

    gzsar_od with free 400

--- script_dual_hmdb51.sh
+++ script_dual_hmdb51.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-#SBATCH --job-name=hmdb_gzsl_od_400
+#SBATCH --job-name=hmdb_gzsl_od_600
 #SBATCH --account=tud01
 #SBATCH --mem=65536
 #SBATCH --partition GpuQ
@@ -9,7 +9,7 @@
 #SBATCH --cpus-per-task=8
 #SBATCH --mail-user=kaiqiang.x.huang@mytudublin.ie
 #SBATCH --mail-type=ALL,TIME_LIMIT_80
-#SBATCH --output=res_dual_hmdb51_gzsl_od_free_400.out
+#SBATCH --output=res_dual_hmdb51_gzsl_od_free_600.out
 
 module load conda/2
 module load cuda/11.2
@@ -17,6 +17,6 @@ module load cuda/11.2
 source activate /ichec/home/users/kaiqiang/py39
 
 echo "GZSL OD: Dual GAN with FREE for HMDB51 (multi-GPU)"
-echo "The number of syn_data is 400"
+echo "The number of syn_data is 600"
 
 time python ./dual/run_hmdb51_tfvaegan_dual.py
