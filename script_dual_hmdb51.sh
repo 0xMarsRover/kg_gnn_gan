6cc1012d2fec08cef3fa0ae11a9a056cd575b9e1
Kaiqiang <kaiqianghuang666@gmail.com>
2022-05-09 01:19:39 +0100

    update

--- script_dual_hmdb51.sh
+++ script_dual_hmdb51.sh
@@ -9,7 +9,7 @@
 #SBATCH --cpus-per-task=8
 #SBATCH --mail-user=kaiqiang.x.huang@mytudublin.ie
 #SBATCH --mail-type=ALL,TIME_LIMIT_80
-#SBATCH --output=res_hmdb51_gzsl_od_600_class_w2v_free_sp_1.out
+#SBATCH --output=res_hmdb51_gzsl_od_600_class_w2v_free_sp_1_15.out
 
 module load conda/2
 module load cuda/11.2
