efa2a370d241efafabc20aceb7fe97db23d18083
Kaiqiang <kaiqianghuang666@gmail.com>
2022-05-27 10:47:50 +0100

    update

--- script_dual_ucf101.sh
+++ script_dual_ucf101.sh
@@ -9,7 +9,7 @@
 #SBATCH --cpus-per-task=8
 #SBATCH --mail-user=kaiqiang.x.huang@mytudublin.ie
 #SBATCH --mail-type=ALL,TIME_LIMIT_80
-#SBATCH --output=res_dual_ucf101_gzsl_od_600_sp_1_10.out
+#SBATCH --output=res_dual_ucf101_gzsl_od_600_sp_7_10.out
 
 module load conda/2
 module load cuda/11.2
