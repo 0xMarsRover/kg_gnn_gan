1ae4749464351097c84536652576fe207af7bc2b
Kaiqiang <kaiqianghuang666@gmail.com>
2022-06-06 09:31:23 +0100

    update

--- scripts/run_ucf101_tfvaegan.py
+++ scripts/run_ucf101_tfvaegan.py
@@ -46,7 +46,7 @@ class_embedding = {'action_class_w2v': 300, 'avg_desc_w2v': 300,
 
 class_embedding = {'action_class_w2v': 300}
 for c, dim in class_embedding.items():
-    for n in range(11, 22):
+    for n in range(22, 31):
         # n = n + 1
         os.system('''CUDA_LAUNCH_BLOCKING=1 python /ichec/work/tucom002c/single_gan/train_tfvaegan.py \
         --dataset ucf101 --nclass_all 101 --gzsl_od --manualSeed 806 \
